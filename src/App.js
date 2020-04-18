ca2f921fd673a85c8d119040da85c34c397146f8
Vinitowinit <vinitparekh24@gmail.com>
2020-04-18 10:45:54 -0700

    Setting up repository on github

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>I am thinking few sections:
+        Home, Content, Porfolio, Resume, Contact
+       </p> 
+
+    <p>
+      Home is a simple image of us 
+      with re-adding code on the bottom 
+      that switches from student...designer...etc
+    </p>
+
+    <p> 
+      This can be as easy as a resume 
+      or as interesing as a road map 
+      of oour Experiences 
+    </p>
+
+    <p>
+      Portfolio is a list of projects that has hovering capability
+      and will route to a new project when clicked
+    </p>
+    <p>
+      Content can pull from our Youtube, Medium, Twitter posts
+      in general 
+    </p>
+    <div> 
+      
+    </div>
     </div>
   );
 }
